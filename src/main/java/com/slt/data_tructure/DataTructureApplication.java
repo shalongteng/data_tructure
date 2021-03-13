9c174fd17047ccc5a0548eb50385e1e284b79b82
shalongteng <1131790681@qq.com>
2021-03-13 09:57:59 +0800

    Initial commit

---
+++ src/main/java/com/slt/data_tructure/DataTructureApplication.java
@@ -0,0 +1,13 @@
+package com.slt.data_tructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataTructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slt/data_tructure/DataTructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slt.data_tructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
